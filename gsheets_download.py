e4c554c12fd31e3f2bcbabbd36fa6170f1bef12d
yogajangkung <31586779+yogajangkung@users.noreply.github.com>
2026-01-01 20:47:25 +0800

    Add files via upload

--- gsheets_download.py
+++ gsheets_download.py
@@ -1,15 +1,42 @@
 import pandas as pd
 
 sheet_id = "15tEvxrNqecs6zzYh8Gc_WcgWGXI_9LHVtNZeUWR_skE"
-sheet_name = "Footystats_Data"   # sheet tujuan FILTER
+sheet_names = ["Score_Filtered"]
 
-url = (
-    f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq"
-    f"?tqx=out:csv"
-    f"&sheet={sheet_name}"
-    f"&range=A1:O1000"
-)
+dfs = []
 
-df = pd.read_csv(url)
-print(f'Extracted {len(df)} rows')
-df.to_csv('footystats_gsheets.csv')
+for sheet in sheet_names:
+    url = (
+        f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq"
+        f"?tqx=out:csv"
+        f"&sheet={sheet}"
+        f"&range=A1:O1000"
+    )
+
+    df = pd.read_csv(url)
+
+    # Ambil kolom berdasarkan POSISI
+    df = df.iloc[:, [0, 1, 8, 9, 10, 12, 14]]
+
+    # RESET NAMA KOLOM (INI KUNCI)
+    df.columns = [
+        "HomeTeam",
+        "AwayTeam",
+        "HomeForm",
+        "AwayForm",
+        "HomeOdds",
+        "DrawOdds",
+        "AwayOdds"
+    ]
+
+    # Optional: tandai asal sheet
+    # df["source"] = sheet
+
+    dfs.append(df)
+
+# CONCAT KE BAWAH (ROW)
+final_df = pd.concat(dfs, axis=0, ignore_index=True)
+
+final_df.to_csv("footystats_combined.csv", index=False)
+
+print("OK. Total rows:", len(final_df))
